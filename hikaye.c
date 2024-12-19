93358fd8a4f3cb17f2932086908a9c35373f07cc
irem044 <iremtopsakal04@gmail.com>
2024-12-19 16:18:02 +0300

    Update hikaye

--- hikaye.c
+++ hikaye.c
@@ -7,5 +7,5 @@ printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş b
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
 printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
 printf("Büyük kardeş küçük kardeşine sakin olmasını ve beraber olurlarsa başlarına kötü bir şey gelmeyeceğini söyledi");//Ömer Faruk Korkunç
-
+printf("Birden arkalarından gelen bir ses onları durdurdu."Kim var orada?"diye bağırdılar ama cevap alamadılar.Birbirlerine korkuyla daha sıkı sarıldılar.")//İrem Topsakal
 }
