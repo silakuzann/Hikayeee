8e7b3b7ff5310b8e490ff2c61762ab4bca8e0340
ibrahimtekk <ibrahimtek006@gmail.com>
2024-12-19 16:09:35 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -6,5 +6,5 @@ int main() {
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
 printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
-
+printf("Bu ses ürkütücü de olsa kardeşlerin dikkatini çekiyordu ve istemsizce onları oraya doğru itiyordu")//İbrahim TEK
 }
